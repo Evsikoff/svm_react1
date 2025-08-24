bf933ab2d883b2672f5609960d12f966ac9aaa2b
Evsikoff <gsmevsikov@gmail.com>
2025-08-24 03:18:50 +0300

    style: center arena components

--- src/Arena.tsx
+++ src/Arena.tsx
@@ -10,7 +10,7 @@ const Arena: React.FC<ArenaProps> = ({ userId }) => {
   if (!userId) return null;
 
   return (
-    <div className="p-8 min-h-[50vh] flex flex-col md:flex-row items-center md:items-start md:space-x-4">
+    <div className="p-8 min-h-[50vh] flex flex-col md:flex-row items-center md:items-start md:space-x-4 md:justify-center">
       <div className="w-full md:w-auto">
         <CompetitionEnergy userId={userId} />
       </div>
